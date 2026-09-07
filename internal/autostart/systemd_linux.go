45dc9c9b1f48711644ad7e0b3f94218b2ceafb8f
kitty <dw_wang126@126.com>
2026-09-07 11:34:43 +0800

    fix(autostart): add Delegate=yes to systemd unit for cgroup permissions
    Without Delegate=yes, rootless podman containers started by the
    systemd user unit cannot be exec'd into -- crun fails with
    "OCI permission denied" when writing to cgroup.procs. This caused
    PGIsReady() to always return false, making pg start --autostart
    hang for 60 seconds before timing out.

--- internal/autostart/systemd_linux.go
+++ internal/autostart/systemd_linux.go
@@ -38,6 +38,7 @@ ConditionPathExists=%s
 
 [Service]
 Type=oneshot
+Delegate=yes
 Environment=PATH=%%h/.local/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin
 ExecStart=%s -c %s start --autostart
 TimeoutStartSec=1800
